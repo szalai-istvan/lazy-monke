d512bf1004eac1ffb9059ef0ca583a67c190505d
szalai-istvan <80052683+szalai-istvan@users.noreply.github.com>
2023-05-09 16:09:41 +0200

    Update Lazy-monke.ps1
    Formatting

--- Lazy-monke.ps1
+++ Lazy-monke.ps1
@@ -1,27 +1,16 @@
 Add-Type -AssemblyName System.Windows.Forms;
 
 echo "            __,__";
-
 echo "   .--.  .-'     '-.  .--.";
-
 echo "  / .. \/  .-. .-.  \/ .. \";
-
 echo " | |  '|  /   Y   \  |'  | |";
-
 echo " | \   \  \ 0 | 0 /  /   / |";
-
 echo "  \ '- ,\.-'`` ``'-./, -' /";
-
 echo "   ''-' /_   ^ ^   _\ '-''";
-
 echo "       |  \._   _./  |";
-
 echo "       \   \ '-' /   /";
-
 echo "        '._ '-=-' _.'";
-
 echo "           '-----'";
-
 echo "";
 
 echo "   Lazy Monke Productions   ";
@@ -41,37 +30,24 @@ echo "Moving cursor and pressing F15 key every second for $seconds seconds, then
 Start-Sleep -Seconds 8;
 
 for ($i = 0; $i -lt $seconds; $i++) {
-
 	Start-Sleep -Seconds 1;	cls;
-
 	echo "$i / $seconds second(s) have passed";
-
   	$Pos = [System.Windows.Forms.Cursor]::Position;
-
 	$x = (($pos.X + 1));
-
   	$y = (($pos.Y + 1));
-
   	[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y);
-
 	$PosNew = [System.Windows.Forms.Cursor]::Position;
-
 	if ($PosNew.X -eq ($x - 1)) {
-
-		$x = 0;		
-
+	$x = 0;
 	}
 
 	if ($PosNew.Y -eq ($y - 1)) {
-
-		$y = 0;		
-
+		$y = 0;	
 	}
 
 	[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y);
 
 	echo "Cursor is moved to ($x,$y)";
-
 	echo "--------------------------";
 
 	[System.Windows.Forms.SendKeys]::SendWait("{F15}");
